f6786e934d57b5db962ab0d3a28cc0834e201a28
jrgerba <119472456+jrgerba@users.noreply.github.com>
2025-05-13 23:33:17 -0400

    Added audio timestamp extraction and return code

--- VidPipe/FFmpeg/AudioExtractor.cs
+++ VidPipe/FFmpeg/AudioExtractor.cs
@@ -7,17 +7,18 @@ public class AudioExtractor(Stream input, string outputFile, AudioCodec codec, u
 {
     public readonly AudioCodec Codec = codec;
     
-    public async Task RunAsync()
+    public async Task<int> RunAsync()
     {
         Process ffmpeg = new()
         {
             StartInfo = new ProcessStartInfo
             {
                 FileName = Path.Combine(Program.AppDataPath, "ffmpeg.exe"),
-                Arguments = $"{startTs ?? "-ss" + startTs} {endTs ?? "-to" + endTs} -i pipe:0 -vn -c:a {Codec.GetCodecName()} -b:a {bitrate} -f {Codec.GetFormat()} pipe:1",
+                //Arguments = $"{(startTs is not null ? $"-ss {startTs} " : "")}{(endTs is not null ? $"-ss {endTs} " : "")}-i pipe:0 -vn -c:a {Codec.GetCodecName()} -b:a {bitrate} -f {Codec.GetFormat()} pipe:1",
+                Arguments = $"{(startTs is not null ? $"-ss {startTs} " : "")}{(endTs is not null ? $"-t {endTs} " : "")}-i pipe:0 -vn -c:a {Codec.GetCodecName()} -b:a {bitrate} -f {Codec.GetFormat()} pipe:1",
                 RedirectStandardInput = true,
                 RedirectStandardOutput = true,
-                RedirectStandardError = true,
+                RedirectStandardError = err is not null,
                 UseShellExecute = false,
                 CreateNoWindow = true
             }
@@ -25,25 +26,33 @@ public class AudioExtractor(Stream input, string outputFile, AudioCodec codec, u
 
         ffmpeg.Start();
         Stream outFile = File.Open(outputFile, FileMode.OpenOrCreate);
-        
-        await Task.WhenAll(
-            Task.Run(async () =>
-            {
-                await input.CopyToAsync(ffmpeg.StandardInput.BaseStream);
-                ffmpeg.StandardInput.Close();
-            }),
-            Task.Run(async () =>
-            {
-                await ffmpeg.StandardOutput.BaseStream.CopyToAsync(outFile);
-                await outFile.FlushAsync();
-            }),
-            Task.Run(async () =>
-            {
-                if (err is not null)
-                    await ffmpeg.StandardError.BaseStream.CopyToAsync(err);
-            })
-        );
+
+        try
+        {
+            await Task.WhenAll(
+                Task.Run(async () =>
+                {
+                    await input.CopyToAsync(ffmpeg.StandardInput.BaseStream);
+                    ffmpeg.StandardInput.Close();
+                }),
+                Task.Run(async () =>
+                {
+                    await ffmpeg.StandardOutput.BaseStream.CopyToAsync(outFile);
+                    await outFile.FlushAsync();
+                }),
+                Task.Run(async () =>
+                {
+                    if (err is not null)
+                        await ffmpeg.StandardError.BaseStream.CopyToAsync(err);
+                })
+            );
+        }
+        catch (Exception e)
+        {
+            // ignored
+        }
 
         await ffmpeg.WaitForExitAsync();
+        return ffmpeg.ExitCode;
     }
 }
