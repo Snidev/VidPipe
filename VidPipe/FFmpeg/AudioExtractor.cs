b546407e04c6cbb6779eb9ca27f33cc2070edf17
jrgerba <119472456+jrgerba@users.noreply.github.com>
2025-05-13 20:40:39 -0400

    Added support for stdout streams from ffmpeg

--- VidPipe/FFmpeg/AudioExtractor.cs
+++ VidPipe/FFmpeg/AudioExtractor.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.IO.Pipelines;
 
 namespace VidPipe.FFmpeg;
 
@@ -13,21 +14,34 @@ public class AudioExtractor(Stream input, string outputFile, AudioCodec codec, u
             StartInfo = new ProcessStartInfo
             {
                 FileName = Path.Combine(Program.AppDataPath, "ffmpeg.exe"),
-                Arguments = $"-i pipe:0 -vn -c:a {codec.GetCodecName()} -b:a {bitrate} {outputFile}",
+                Arguments = $"-i pipe:0 -vn -c:a {Codec.GetCodecName()} -b:a {bitrate} -f {Codec.GetFormat()} pipe:1",
                 RedirectStandardInput = true,
-                //RedirectStandardOutput = true,
-                RedirectStandardError = err is not null,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
                 UseShellExecute = false,
                 CreateNoWindow = true
             }
         };
 
         ffmpeg.Start();
-
-        await input.CopyToAsync(ffmpeg.StandardInput.BaseStream);
-        ffmpeg.StandardInput.Close();
-        if (err is not null)
-            await ffmpeg.StandardError.BaseStream.CopyToAsync(err);
+        Stream outFile = File.Open(outputFile, FileMode.OpenOrCreate);
+        
+        await Task.WhenAll(
+            Task.Run(async () =>
+            {
+                await input.CopyToAsync(ffmpeg.StandardInput.BaseStream);
+                ffmpeg.StandardInput.Close();
+            }),
+            Task.Run(async () =>
+            {
+                await ffmpeg.StandardOutput.BaseStream.CopyToAsync(outFile);
+            }),
+            Task.Run(async () =>
+            {
+                if (err is not null)
+                    await ffmpeg.StandardError.BaseStream.CopyToAsync(err);
+            })
+        );
 
         await ffmpeg.WaitForExitAsync();
     }
