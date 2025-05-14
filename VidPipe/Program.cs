942832f8b000ed10db5a30f31da7d15ff287d16c
jrgerba <119472456+jrgerba@users.noreply.github.com>
2025-05-13 21:50:03 -0400

    Set up CLI environment

--- VidPipe/Program.cs
+++ VidPipe/Program.cs
@@ -2,9 +2,7 @@
 using System.CommandLine;
 using System.Diagnostics;
 using VidPipe.FFmpeg;
-using YoutubeExplode;
-using YoutubeExplode.Videos;
-using YoutubeExplode.Videos.Streams;
+using VidPipe.YoutubeExplode;
 
 namespace VidPipe;
 
@@ -13,56 +11,77 @@ class Program
     public static string AppDataPath => Path.Combine(GetFolderPath(SpecialFolder.ApplicationData),
         "VidPipe");
 
-    private static string? VideoId;
+    private static async Task GetAudioSingle(string link, string outFile)
+    {
+        Console.WriteLine($"Beginning download of {link}");
+        AudioDownloadJob download = await AudioDownloadJob.CreateAudioStream(link);
+        AudioExtractor audio = new(download.Stream, outFile, AudioCodec.Mp3, 193000);
+
+        try
+        {
+            await Task.WhenAll(
+                audio.RunAsync(),
+                Task.Run(async () =>
+                {
+                    while (download.BytesWritten < download.TotalBytes)
+                    {
+                        Console.CursorLeft = 0;
+                        Console.Write($"Download Progress: {download.BytesWritten} B / {download.TotalBytes} B");
+                        await Task.Delay(100);
+                    }
+
+                    Console.WriteLine($"\nDownload Complete ({download.TotalBytes} B)");
+                })
+            );
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failed to download {link}");
+            return;
+        }
+        finally
+        {
+            await download.DisposeAsync();
+        }
+
+        Console.WriteLine($"Successfully output to {outFile}");
+    }
+
+    private static void BulkDownload(string csv, string outFolder)
+    {
+        
+    }
+
+    private static RootCommand SetupCommands()
+    {
+        RootCommand root = new("A utility for downloading YouTube videos in various formats");
+        
+        Command audioCmd = new("audio", "Download the audio of a single video in mp3 format");
+        Option<string> videoOpt = new(aliases: ["--video", "-v"], description: "The id or link to a youtube video")
+        {
+            IsRequired = true
+        };
+
+        Option<string> outOpt = new(aliases: ["--output", "-o"], description: "The name and directory of the output file")
+        {
+            IsRequired = true
+        };
+        
+        audioCmd.Add(videoOpt);
+        audioCmd.Add(outOpt);
+        
+        audioCmd.SetHandler(GetAudioSingle, videoOpt, outOpt);
+
+        root.Add(audioCmd);
+        return root;
+    }
     
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
         if (!Directory.Exists(AppDataPath))
             Directory.CreateDirectory(AppDataPath);
 
-        RootCommand cmd = new("YouTube audio downloader");
-
-        Option<string> videoOpt = new(aliases: ["--video", "-v"],
-            description: "The id or link to a youtube video")
-        {
-            IsRequired = true
-        };
-
-        Option<string> outOpt = new(aliases: ["--output", "-o"],
-            description: "The name and directory of the output file")
-        {
-            IsRequired = true
-        };
-        
-        cmd.Add(videoOpt);
-        cmd.Add(outOpt);
-
-        string url = "", outDir = "";
-        cmd.SetHandler((urlVar, outDirVar) =>
-        {
-            url = urlVar;
-            outDir = outDirVar;
-        }, videoOpt, outOpt);
-
-        await cmd.InvokeAsync(args);
-        
-        const string prefix = "https://www.youtube.com/watch?v=";
-        url = url.StartsWith(prefix) ? url : prefix + url;
-
-        Console.WriteLine($"Downloading audio from {url}");
-            
-        YoutubeClient ytc = new();
-        Video video = await ytc.Videos.GetAsync(url);
-        StreamManifest manifest = await ytc.Videos.Streams.GetManifestAsync(video.Id);
-        IStreamInfo si = manifest.GetAudioOnlyStreams().GetWithHighestBitrate();
-
-        Stream videoStream = await ytc.Videos.Streams.GetAsync(si);
-        IFfJob extractor = new AudioExtractor(videoStream, outDir, AudioCodec.Mp3, 192_000);
-
-        await extractor.RunAsync();
-            
-        /*ytc.Videos.Streams.CopyToAsync(si, ffmpeg.StandardInput.BaseStream).GetAwaiter().GetResult();*/
-            
-        Console.WriteLine($"Saved output to {outDir}");
+        RootCommand root = SetupCommands();
+        root.Invoke(args);
     }
 }
