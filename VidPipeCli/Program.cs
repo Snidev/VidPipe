014447c50f05660a1de301fe66c552e73338d5c9
jrgerba <119472456+jrgerba@users.noreply.github.com>
2025-05-17 23:47:27 -0400

    Separated CLI into separate project

---
+++ VidPipeCli/Program.cs
@@ -0,0 +1,20 @@
+﻿using VidPipe;
+
+namespace VidPipeCli;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        /*YoutubeInterface yt = new();
+        Stream video = await yt.GetAudioStream("https://www.youtube.com/watch?v=zhKB78Cm_44");
+
+        Stream file = File.Open(@"C:\Users\Joe\Desktop\vid", FileMode.OpenOrCreate);
+        await video.CopyToAsync(file);
+        video.Close();
+        file.Close();
+        yt.Dispose();*/
+
+        await MainProgram.ProcessJobBatchFromCsv(@"C:\Users\Joe\Desktop\suite1.csv", @"C:\Users\Joe\Desktop\files");
+    }
+}
